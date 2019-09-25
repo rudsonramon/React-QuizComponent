0e534f2270442a4430740a07447fa8118ba58413
Rodrigues <rudson.r.rodrigues@accenture.com>
2019-09-25 20:36:53 -0300

    Completed module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
-import QuizQuestion from './Quiz.js'
+import Quiz from './Quiz.js'
 
 class App extends Component {
   render() {
     return (
-      <QuizQuestion />
+      <Quiz />
     )
   }
 }
