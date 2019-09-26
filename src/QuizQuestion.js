25b44d1cf25b7746e42286adce2c82348f4373cf
Rodrigues <rudson.r.rodrigues@accenture.com>
2019-09-25 21:16:27 -0300

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -13,7 +13,7 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <li>{this.props.quiz_question.answer_option[0]}</li>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
           </ul>
         </section>
       </main>
