84b51a58f3ef60ae9ab79c3499fdf84eb406ebc8
Rodrigues <rudson.r.rodrigues@accenture.com>
2019-09-25 21:12:57 -0300

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,8 @@ class Quiz extends Component {
   render() {
     return (
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_question -1]}/>
+        <QuizQuestion quiz_question={quizData.quiz_questions
+          [this.state.quiz_position -1]}/>
       </div> 
     )
   }
