64ebc991919d998efe7b19c373e8293c368a75ee
Rodrigues <rudson.r.rodrigues@accenture.com>
2019-09-26 21:22:52 -0300

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,7 +7,7 @@ class QuizQuestion extends Component {
     this.state = { quiz_position: 1 }
   }
   handleClick(buttonText) {
-    if(buttonText === this.props.quiz_question.answer_options) {
+    if(buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler()
     }
   }
