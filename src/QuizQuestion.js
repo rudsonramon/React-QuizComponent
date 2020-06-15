4716e50fff79b1da06b20f7bc7f10e15048ff4e6
Rodrigues <rudson.r.rodrigues@accenture.com>
2020-06-14 23:11:31 -0300

    test

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,7 +7,7 @@ class QuizQuestion extends Component {
     this.state = { quiz_position: 1 }
   }
   handleClick(buttonText) {
-    if(buttonText === this.props.quiz_question.answer) {
+    if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler()
     }
   }
